e02bbfc98d27d0eec240af51513de928c84235a7
saribence <saribence10@gmail.com>
2025-11-27 14:27:00 +0100

    Az oldal kiválasztása - Delfood

---
+++ src/main/java/com/example/gyakbea/GyakbeaApplication.java
@@ -0,0 +1,13 @@
+package com.example.gyakbea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyakbeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GyakbeaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gyakbea/GyakbeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gyakbea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyakbeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
